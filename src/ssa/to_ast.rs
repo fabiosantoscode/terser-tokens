925206343191a68245324f40978150a982317322
Fábio Santos <fabiosantosart@gmail.com>
2023-06-18 20:42:43 +0100

    fix branches

--- src/ssa/to_ast.rs
+++ src/ssa/to_ast.rs
@@ -4,6 +4,7 @@ use super::ssa_fn::SsaFn;
 use domtree::frontier::DominanceFrontier;
 use domtree::DomTree;
 use std::collections::{HashMap, HashSet};
+use std::fmt::{Debug, Formatter};
 use swc_ecma_ast::{Expr, Ident, ReturnStmt, Stmt};
 
 impl SsaFn {
@@ -12,7 +13,7 @@ impl SsaFn {
     }
 }
 
-#[derive(Debug, Default, Clone)]
+#[derive( Default, Clone)]
 enum StructuredFlow {
     #[default]
     Nop,
@@ -22,11 +23,89 @@ enum StructuredFlow {
         Vec<StructuredFlow>,
         Vec<StructuredFlow>,
     ),
-    Break,
+    Break(usize),
     Loop(Vec<StructuredFlow>),
     Block(Vec<StructuredFlow>),
     Return(ExitType),
     SsaBlock(usize),
+    SsaRef(usize)
+}
+
+impl StructuredFlow {
+    fn str_head(&self) -> String {
+        match self {
+            StructuredFlow::Nop => "Nop".to_string(),
+            StructuredFlow::Straight(_) => "Straight".to_string(),
+            StructuredFlow::Branch(_, _, _) => "Branch".to_string(),
+            StructuredFlow::Break(_) => "Break".to_string(),
+            StructuredFlow::Loop(_) => "Loop".to_string(),
+            StructuredFlow::Block(_) => "Block".to_string(),
+            StructuredFlow::Return(_) => "Return".to_string(),
+            StructuredFlow::SsaBlock(_) => "SsaBlock".to_string(),
+            StructuredFlow::SsaRef(_) => "SsaRef".to_string(),
+        }
+    }
+    fn children(&self) -> Vec<Vec<StructuredFlow>> {
+        match self {
+            StructuredFlow::Nop => vec![],
+            StructuredFlow::Straight(x) => vec![x.clone()],
+            StructuredFlow::Branch(x, y, z) => vec![x.clone(), y.clone(), z.clone()],
+            StructuredFlow::Break(_) => vec![],
+            StructuredFlow::Loop(x) => vec![x.clone()],
+            StructuredFlow::Block(x) => vec![x.clone()],
+            StructuredFlow::Return(_) => vec![],
+            StructuredFlow::SsaBlock(_) => vec![],
+            StructuredFlow::SsaRef(_) => vec![],
+        }
+    }
+    fn index(&self) -> Option<usize> {
+        match self {
+            StructuredFlow::Nop => None,
+            StructuredFlow::Straight(_) => None,
+            StructuredFlow::Branch(_, _, _) => None,
+            StructuredFlow::Break(x) => Some(*x),
+            StructuredFlow::Loop(_) => None,
+            StructuredFlow::Block(_) => None,
+            StructuredFlow::Return(_) => None,
+            StructuredFlow::SsaBlock(x) => Some(*x),
+            StructuredFlow::SsaRef(x) => Some(*x),
+        }
+    }
+}
+
+impl Debug for StructuredFlow {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+                let indent_str_lines = |s: &str| {
+                    let lines = s.lines();
+                    let indented_lines = lines.map(|line| format!("    {}", line));
+                    indented_lines.collect::<Vec<String>>().join("\n")
+                };
+
+        if let Some(index) = self.index() {
+            write!(f, "{}({})", self.str_head(), index)
+        } else {
+            let children = self.children();
+            if children.len() == 0 {
+                write!(f, "{}", self.str_head())
+            } else if children.len() == 1 {
+                let children = format!("{:?}", children[0]);
+                let is_multiline = children.contains("\n");
+                if is_multiline {
+                    let lines = indent_str_lines(&children);
+                    return write!(f, "{}(\n{}\n)", self.str_head(), lines);
+                }
+                write!(f, "{}({})", self.str_head(), children)
+            } else {
+                let lines = children
+                    .iter()
+                    .map(|child| indent_str_lines(&format!("{:?}", child)))
+                    .collect::<Vec<String>>()
+                    .join("\n");
+
+                write!(f, "{}(\n{}\n)", self.str_head(), lines)
+            }
+        }
+    }
 }
 
 // type Context = [ContainingSyntax] -- innermost form first
@@ -55,7 +134,7 @@ fn do_tree(func: &SsaFn)  -> StructuredFlow{
 }
 
 fn do_tree_inner(graph: &Graph, node: usize, context: Ctx) -> StructuredFlow {
-    let code_for_x = |context: Ctx| {
+    let code_for_node = |context: Ctx| {
         node_within(
             graph,
             node,
@@ -75,9 +154,9 @@ fn do_tree_inner(graph: &Graph, node: usize, context: Ctx) -> StructuredFlow {
         inner_context
             .containing_syntax
             .push(ContainingSyntax::LoopHeadedBy(node));
-        StructuredFlow::Loop(vec![code_for_x(inner_context)])
+        StructuredFlow::Loop(vec![code_for_node(inner_context)])
     } else {
-        code_for_x(context)
+        code_for_node(context)
     }
 }
 
@@ -96,7 +175,7 @@ fn node_within(graph: &Graph, node: usize, ys: Vec<&usize>, context: Ctx) -> Str
                         inner_context
                     };
                     StructuredFlow::Branch(
-                        tx_block(graph, node, context.clone()),
+                        tx_expr(graph, *e),
                         vec![do_branch(graph, node, *t, inner_context.clone())],
                         vec![do_branch(graph, node, *f, inner_context.clone())],
                     )
@@ -125,8 +204,8 @@ fn tx_block(graph: &Graph, node: usize, context: Ctx) -> Vec<StructuredFlow> {
     vec![StructuredFlow::SsaBlock(node)]
 }
 
-fn tx_expr(node: usize, expr: Expr) -> Vec<StructuredFlow> {
-    vec![StructuredFlow::SsaBlock(node)]
+fn tx_expr(graph: &Graph, var_index: usize) -> Vec<StructuredFlow> {
+    vec![StructuredFlow::SsaRef(var_index)]
 }
 
 fn do_branch(graph: &Graph, source: usize, target: usize, context: Ctx) -> StructuredFlow {
@@ -141,7 +220,7 @@ fn do_branch(graph: &Graph, source: usize, target: usize, context: Ctx) -> Struc
     } else if
     /* multiple nodes come here */
     graph.nodes[target].incoming_edges.len() > 1 {
-        StructuredFlow::Break // TODO "WasmBr i -- exit"
+        StructuredFlow::Break(index_target) // TODO "WasmBr i -- exit"
     } else {
         do_tree_inner(graph, target, context)
     }
@@ -182,24 +261,29 @@ mod tests {
 
     #[test]
     fn mk_stats() {
-        let func = test_ssa_block("123 ? 456 : 789");
+        let func = test_ssa_block("123; 123; 123 ? 456 : 789");
         insta::assert_debug_snapshot!(func, @r###"
         @0: {
             $0 = 123
-            exit = cond $0 ? jump @1 : jump @2
+            $1 = 123
+            exit = jump @1
         }
         @1: {
-            $1 = 456
-            exit = jump @3
+            $2 = 123
+            exit = cond $2 ? jump @2 : jump @3
         }
         @2: {
-            $2 = 789
-            exit = jump @3
+            $3 = 456
+            exit = jump @4
         }
         @3: {
-            $3 = either($1, $2)
-            $4 = undefined
-            exit = return $4
+            $4 = 789
+            exit = jump @4
+        }
+        @4: {
+            $5 = either($3, $4)
+            $6 = undefined
+            exit = return $6
         }
         "###);
 
@@ -210,38 +294,13 @@ mod tests {
         let stats = do_tree(&func);
         insta::assert_debug_snapshot!(stats, @r###"
         Block(
-            [
-                SsaBlock(
-                    0,
-                ),
-                Branch(
-                    [
-                        SsaBlock(
-                            0,
-                        ),
-                    ],
-                    [
-                        Block(
-                            [
-                                SsaBlock(
-                                    1,
-                                ),
-                                Break,
-                            ],
-                        ),
-                    ],
-                    [
-                        Block(
-                            [
-                                SsaBlock(
-                                    2,
-                                ),
-                                Break,
-                            ],
-                        ),
-                    ],
-                ),
-            ],
+            [SsaBlock(0), Block(
+                [SsaBlock(1), Branch(
+                    [SsaRef(2)]
+                    [Block([SsaBlock(2), Break(0)])]
+                    [Block([SsaBlock(3), Break(0)])]
+                )]
+            )]
         )
         "###);
     }
@@ -291,64 +350,17 @@ mod tests {
         let stats = do_tree(&func);
         insta::assert_debug_snapshot!(stats, @r###"
         Block(
-            [
-                SsaBlock(
-                    0,
-                ),
-                Branch(
-                    [
-                        SsaBlock(
-                            0,
-                        ),
-                    ],
-                    [
-                        Block(
-                            [
-                                SsaBlock(
-                                    1,
-                                ),
-                                Branch(
-                                    [
-                                        SsaBlock(
-                                            1,
-                                        ),
-                                    ],
-                                    [
-                                        Block(
-                                            [
-                                                SsaBlock(
-                                                    2,
-                                                ),
-                                                Break,
-                                            ],
-                                        ),
-                                    ],
-                                    [
-                                        Block(
-                                            [
-                                                SsaBlock(
-                                                    3,
-                                                ),
-                                                Break,
-                                            ],
-                                        ),
-                                    ],
-                                ),
-                            ],
-                        ),
-                    ],
-                    [
-                        Block(
-                            [
-                                SsaBlock(
-                                    6,
-                                ),
-                                Break,
-                            ],
-                        ),
-                    ],
-                ),
-            ],
+            [SsaBlock(0), Branch(
+                [SsaRef(0)]
+                [Block(
+                    [SsaBlock(1), Branch(
+                        [SsaRef(1)]
+                        [Block([SsaBlock(2), Break(3)])]
+                        [Block([SsaBlock(3), Break(3)])]
+                    )]
+                )]
+                [Block([SsaBlock(6), Break(0)])]
+            )]
         )
         "###);
     }
@@ -405,73 +417,21 @@ mod tests {
         let stats = do_tree(&func);
         insta::assert_debug_snapshot!(stats, @r###"
         Block(
-            [
-                SsaBlock(
-                    0,
-                ),
-                Block(
-                    [
-                        SsaBlock(
-                            1,
-                        ),
-                        Branch(
-                            [
-                                SsaBlock(
-                                    1,
-                                ),
-                            ],
-                            [
-                                Block(
-                                    [
-                                        SsaBlock(
-                                            2,
-                                        ),
-                                        Block(
-                                            [
-                                                SsaBlock(
-                                                    3,
-                                                ),
-                                                Branch(
-                                                    [
-                                                        SsaBlock(
-                                                            3,
-                                                        ),
-                                                    ],
-                                                    [
-                                                        Block(
-                                                            [
-                                                                SsaBlock(
-                                                                    4,
-                                                                ),
-                                                                Break,
-                                                            ],
-                                                        ),
-                                                    ],
-                                                    [
-                                                        Break,
-                                                    ],
-                                                ),
-                                            ],
-                                        ),
-                                    ],
-                                ),
-                            ],
-                            [
-                                Block(
-                                    [
-                                        SsaBlock(
-                                            9,
-                                        ),
-                                        Return(
-                                            Return,
-                                        ),
-                                    ],
-                                ),
-                            ],
-                        ),
-                    ],
-                ),
-            ],
+            [SsaBlock(0), Block(
+                [SsaBlock(1), Branch(
+                    [SsaRef(0)]
+                    [Block(
+                        [SsaBlock(2), Block(
+                            [SsaBlock(3), Branch(
+                                [SsaRef(1)]
+                                [Block([SsaBlock(4), Break(3)])]
+                                [Break(2)]
+                            )]
+                        )]
+                    )]
+                    [Block([SsaBlock(9), Return])]
+                )]
+            )]
         )
         "###);
     }
