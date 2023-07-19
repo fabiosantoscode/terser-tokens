38ce8894c899ef27a692f9c73ed0277e3056a833
Fábio Santos <fabiosantosart@gmail.com>
2023-07-19 18:09:59 +0100

    do not clone HashMap with variables in our scope. Use a hashed-array-mapped-trie instead

--- src/scope/scope.rs
+++ src/scope/scope.rs
@@ -1,9 +1,9 @@
-use std::collections::HashMap;
+use hamt::HamtRc;
 
-#[derive(Debug, Default, Clone)]
+#[derive(Clone)]
 pub struct Scope {
     pub parent: Option<Box<Scope>>,
-    pub vars: HashMap<String, usize>,
+    pub vars: HamtRc<String, usize>,
     pub is_block: bool,
 }
 
@@ -11,7 +11,8 @@ impl Scope {
     pub fn new(is_block: bool) -> Self {
         Self {
             is_block,
-            ..Default::default()
+            vars: HamtRc::new(),
+            parent: None,
         }
     }
 
@@ -28,7 +29,7 @@ impl Scope {
     }
 
     pub fn insert(&mut self, name: String, idx: usize) {
-        self.vars.insert(name, idx);
+        self.vars = self.vars.insert(&name, &idx);
     }
 
     pub fn go_into_block(&mut self) -> Self {
@@ -41,5 +42,52 @@ impl Scope {
         let mut new_scope = Self::new(false);
         new_scope.parent = Some(Box::new(self.clone()));
         new_scope
+    }
+
+    pub fn leave(&mut self) -> Self {
+        if let Some(parent) = &self.parent {
+            return *parent.clone();
+        }
+
+        self.clone()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_get_set() {
+        let mut scope = Scope::new(false);
+        scope.insert("a".to_string(), 0);
+        scope.insert("b".to_string(), 1);
+        scope.insert("c".to_string(), 2);
+
+        assert_eq!(scope.get("a"), Some(0));
+        assert_eq!(scope.get("b"), Some(1));
+        assert_eq!(scope.get("c"), Some(2));
+        assert_eq!(scope.get("d"), None);
+    }
+
+    #[test]
+    fn can_get_set_in_block() {
+        let mut scope = Scope::new(true);
+        scope.insert("a".to_string(), 0);
+        let mut scope = scope.go_into_block();
+        scope.insert("b".to_string(), 1);
+        scope.insert("c".to_string(), 2);
+
+        assert_eq!(scope.get("a"), Some(0));
+        assert_eq!(scope.get("b"), Some(1));
+        assert_eq!(scope.get("c"), Some(2));
+        assert_eq!(scope.get("d"), None);
+
+        let mut scope = scope.leave();
+
+        assert_eq!(scope.get("a"), Some(0));
+        assert_eq!(scope.get("b"), None);
+        assert_eq!(scope.get("c"), None);
+        assert_eq!(scope.get("d"), None);
     }
 }
