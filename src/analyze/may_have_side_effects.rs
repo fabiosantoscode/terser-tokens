9636da33a8f15158492712c1640c47f4d52373a1
Fábio Santos <fabiosantosart@gmail.com>
2023-08-12 15:31:47 +0100

    add new may_have_side_effects method

---
+++ src/analyze/may_have_side_effects.rs
@@ -0,0 +1,23 @@
+use crate::basic_blocks::BasicBlockInstruction;
+
+impl BasicBlockInstruction {
+    pub fn may_have_side_effects(&self) -> bool {
+        match self {
+            BasicBlockInstruction::LitNumber(_) => false,
+            BasicBlockInstruction::Ref(_) => false,
+            BasicBlockInstruction::BinOp(_, _, _) => true, // may throw due to bigint
+            BasicBlockInstruction::Undefined => false,
+            BasicBlockInstruction::This => false,
+            BasicBlockInstruction::CaughtError => false,
+            BasicBlockInstruction::Array(_) => false,
+            BasicBlockInstruction::TempExit(_, _) => true,
+            BasicBlockInstruction::Phi(_) => true, // don't mess with phi
+            BasicBlockInstruction::Function(_) => false,
+            BasicBlockInstruction::Call(_, _) => true,
+            BasicBlockInstruction::ArgumentRead(_) => false,
+            BasicBlockInstruction::ArgumentRest(_) => false,
+            BasicBlockInstruction::ReadNonLocal(_) => false,
+            BasicBlockInstruction::WriteNonLocal(_, _) => true,
+        }
+    }
+}
--- src/analyze/mod.rs
+++ src/analyze/mod.rs
@@ -1,5 +1,7 @@
 mod can_be_reordered;
 mod count_variable_uses;
+mod may_have_side_effects;
 
 pub use can_be_reordered::*;
 pub use count_variable_uses::*;
+pub use may_have_side_effects::*;
